284bd064f51459ef8adb301d88c6384d0b4dcba7
Jose Carlos Nuñez Valverde <jnunez@proinsalat.com>
2025-07-30 09:21:42 -0600

    nueva estructura de folders - add taller_css

---
+++ taller_css/main.js
@@ -0,0 +1,15 @@
+const btnNoticia1 = document.getElementById("btnNoticia1");
+
+if (btnNoticia1) {
+  btnNoticia1.addEventListener("click", () => {
+    alert('¡Has hecho clic en "Leer más" de la Noticia 1!');
+  });
+}
+
+const btnNoticia2 = document.getElementById("btnNoticia2");
+
+if (btnNoticia2) {
+  btnNoticia2.addEventListener("click", () => {
+    alert('¡Has hecho clic en "Ver detalles" de la Noticia 2!');
+  });
+}
